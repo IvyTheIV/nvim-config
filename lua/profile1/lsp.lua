dab0c38ac7c402da63fed299894997da5f0df1e4
IvyTheIV <ivythe4@proton.me>
2024-02-14 15:38:40 +0600

    clangd: disable snippets, header insertion

--- lua/profile1/lsp.lua
+++ lua/profile1/lsp.lua
@@ -62,7 +62,9 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-    -- clangd = {},
+    clangd = {
+        enable_snippets = false,
+    },
     -- gopls = {},
     -- pyright = {},
     rust_analyzer = {},
@@ -98,13 +100,22 @@ mason_lspconfig.setup {
     ensure_installed = vim.tbl_keys(servers),
 }
 
+local lspconfig = require('lspconfig');
+
 mason_lspconfig.setup_handlers {
     function(server_name)
-        require('lspconfig')[server_name].setup {
+        lspconfig[server_name].setup {
             capabilities = capabilities,
             on_attach = on_attach,
             settings = servers[server_name],
             filetypes = (servers[server_name] or {}).filetypes,
         }
     end,
+}
+
+lspconfig["clangd"].setup {
+    cmd = {
+        "clangd",
+        "-header-insertion=never",
+    },
 }
